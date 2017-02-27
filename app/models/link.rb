e9541fba6fc58939641e866f59499ad191b63ce1
Vido <vidoseaver@gmail.com>
2017-02-27 10:36:11 -0700

    Add links model and migration

---
+++ app/models/link.rb
@@ -0,0 +1,16 @@
+require "uri"
+class Link < ApplicationRecord
+  belongs_to :user
+
+  def self.valid_url?(url)
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) && !uri.host.nil? ? true : false
+  end
+
+  def self.top_ten_read_in_last_24_hours
+    links = Link.where(read: true)
+    .where(updated_at: (Time.now - 24.hours)..Time.now)
+    grouped = links.group_by{|link| link.url}
+    grouped.values.map {|similiar_links| similiar_links.first}.take(10)
+  end
+end
