d30319f0e196979c080f28c88671a64ea7665292
Vido <vidoseaver@gmail.com>
2017-02-27 10:31:44 -0700

    Add Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+
+  root to: "sessions#new"
+  resources :links,     only: [:index, :create, :edit, :update]
+  resources :sessions,  only: [:new, :create, :destroy]
+  resources :users,     only: [:new, :create]
+
+  namespace :api do
+    namespace :v1 do
+      resources :links,     only: [:index, :update]
+    end
+  end
+
+
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
